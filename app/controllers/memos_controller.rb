04ac04abd97746b592311c1743da03b80dec00d0
EC2 Default User <ec2-user@ip-172-31-30-144.us-east-2.compute.internal>
2018-09-30 18:01:56 +0000

    initial commit

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,37 @@
+class MemosController < ApplicationController
+    #メモ一覧ページ
+    def index
+        #トップページ
+        @memos = Memo.all
+    
+    end
+    def new
+        #新規追加ボタンクリック時のアクション
+        
+    end
+    
+    #メモ新規追加ページ
+    def add
+        #新規追加ページでpost時のアクション
+        Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
+        redirect_to "/"
+    end
+    
+    #メモ編集ページ
+    def edit
+        @memo = Memo.find(params["id"])
+    
+    end
+    def update
+        update_memo = Memo.find(params["id"])
+        update_memo.title = params["memos"]["title"]
+        update_memo.body = params["memos"]["body"]
+        update_memo.save
+        redirect_to "/"
+    end
+    def destroy
+        delete_memo = Memo.find(params["id"])
+        delete_memo.destroy
+        redirect_to "/"
+    end
+end
